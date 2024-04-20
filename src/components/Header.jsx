10046dbb83392af382d0b101c24fb6db0f0fd399
Sarthak Gupta <smartsarthakgupta12345@gmail.com>
2024-04-20 17:53:21 +0530

    voting link added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -113,6 +113,14 @@ import Contact from './Contact';
                                         </li>
                                         <li>
                                             <HashLink
+                                                to={"https://www.google.com"}
+                                                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                                            >
+                                                Voting
+                                            </HashLink>
+                                        </li>
+                                        <li>
+                                            <HashLink
                                                 to={"/contact"}
                                                 className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                                             >
