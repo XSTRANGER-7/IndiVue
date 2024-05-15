784947761966b2f27ce0e2ffa5e414c91726fadd
Sarthak Gupta <smartsarthakgupta12345@gmail.com>
2024-05-15 20:59:12 +0530

    logo updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {HashLink} from 'react-router-hash-link';
+import img from "../assets/positive-vote.png";
 import Contact from './Contact';
 
             function Header() {
@@ -9,12 +10,12 @@ import Contact from './Contact';
                             <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                                 <a href="/" className="flex items-center">
                                     <img
-                                        src="https://flowbite.com/docs/images/logo.svg"
+                                        src={img}
                                         className="mr-3 h-6 sm:h-9"
                                         alt="Flowbite Logo"
                                     />
                                     <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-                                        Agora Cognito
+                                    IndiVue
                                     </span>
                                 </a>
                                 <div className="flex items-center lg:order-2">
